0ac4c41eb0b7b8b60596f041ac9a66de6fc6a6ab
Syed Muhammad Sohaib <33151569+syedMSohaib@users.noreply.github.com>
2022-08-30 15:29:32 +0500

    added debouncing

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react'
 import './Header.css';
 
 export default function Header(): JSX.Element {
-  return (
+
+    const [searchText, setsearchText] = useState("")
+
+    const debounceFunction = (fn: any, d: string|number) => {
+        let timer: any;
+        return function() {
+            let self = this;
+            let args = arguments;
+            clearInterval(timer);
+            timer = setTimeout(() => {
+                fn.apply(self, arguments);
+            }, d);
+        }
+    }
+
+    const debounceFunc = useCallback(debounceFunction((value: any) => filterMovie(value), 700), []);
+
+    const searchMovies = (value: any) => {
+        setsearchText(value);
+        debounceFunc(value);
+    }
+
+    const filterMovie = (value: string|number) => {
+
+        let tosearch = searchText ? searchText : value;
+
+        console.log(searchText, value);
+
+        //now send it to parent component so that filter can be done
+    }
+
+    return (
     <section id="navBar">
         <div className="container-fluid p-0">
             <div className="row no-gutters justify-content-center">
@@ -12,7 +43,10 @@ export default function Header(): JSX.Element {
                             <img src="/images/logo.png" alt="logo" className="navbarBrand" />
                         </a>
                         <div className="searchbox">
-                            <input type="text" name="search" id="nav_search" placeholder="Search title" />
+                            <input type="text"
+                            name="search" id="nav_search" placeholder="Search title"
+                            onKeyUp={(e) => searchMovies(e.target.value) } />
+
                         </div>
                     </nav>
                 </div>
